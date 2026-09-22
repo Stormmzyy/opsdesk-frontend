e8aabff364824fd472faedd9c2995fc2b05329ef
Stormmzyy <tomiwoogundare0@gmail.com>
2026-09-22 15:25:17 +0200

    Move user components into components/users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import EmployeeDirectory from './components/employees/EmployeeDirectory.jsx'
 import TicketBoard from './components/tickets/TicketBoard.jsx'
-import UserDirectory from './components/UserDirectory.jsx'
+import UserDirectory from './components/users/UserDirectory.jsx'
 
 function App() {
   // Temporary stacked layout; a proper page layout comes later.
